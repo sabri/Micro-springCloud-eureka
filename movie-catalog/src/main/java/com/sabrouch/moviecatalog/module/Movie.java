97574375072c6b8aebadf6c626c0a3bba7a3cf34
sabri baazaoui <baazaouisabri@gmail.com>
2020-11-28 23:28:32 +0100

    regular eureuka

---
+++ movie-catalog/src/main/java/com/sabrouch/moviecatalog/module/Movie.java
@@ -0,0 +1,15 @@
+package com.sabrouch.moviecatalog.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    private String movieId;
+    private String name;
+
+}
